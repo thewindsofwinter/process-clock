d6d350165b6900daa835266d05106186968556f8
Andy Tang <winterwind2022@gmail.com>
2020-06-28 19:24:59 -0500

    initial commit with basic functionality

---
+++ ProcessClock/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ProcessClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Log
+            // 
+            this.Log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Log.Location = new System.Drawing.Point(0, 0);
+            this.Log.Multiline = true;
+            this.Log.Name = "Log";
+            this.Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Log.Size = new System.Drawing.Size(800, 450);
+            this.Log.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Log);
+            this.Name = "Form1";
+            this.Text = "Windows Process Clock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Log;
+    }
+}
+
---
+++ ProcessClock/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessClock
+{
+    public partial class Form1 : Form
+    {
+        WinEventDelegate dele = null;
+        DateTime curr = DateTime.Now;
+        Dictionary<String, TimeSpan> dict = null;
+        String currprocess = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dict = new Dictionary<String, TimeSpan>();
+            currprocess = "ProcessClock";
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        string GetActiveProcessName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+
+            return p.ProcessName;
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            String s = GetActiveProcessName();
+            DateTime now = DateTime.Now;
+
+            System.TimeSpan diff = DateTime.Now.Subtract(curr);
+            if (dict.ContainsKey(currprocess))
+            {
+                dict[currprocess].Add(diff);
+            }
+            else
+            {
+                dict.Add(currprocess, diff);
+            }
+            Log.Text += "Successfully added duration " + diff + " to process " + currprocess + "\r\n";
+
+            // If one opens the ProcessClock window, the program will automatically write all data to the day's file
+            if (s.Equals("ProcessClock"))
+            {
+                System.IO.File.WriteAllText(@"C:\Users\winterwind\Desktop\" + now.Month + "-" + now.Day + ".txt", String.Empty);
+                using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\Users\winterwind\Desktop\" + now.Month + "-" + now.Day + ".txt"))
+                {
+                    file.WriteLine("Time spent on processes:");
+                    foreach (String process in dict.Keys)
+                    {
+                        if (!process.Equals("ProcessClock"))
+                        {
+                            file.WriteLine(process + ": " + dict[process]);
+                        }
+                    }
+                }
+                Log.Text += "Successfully wrote data to file!\r\n";
+            }
+
+            currprocess = s;
+            curr = now;
+        }
+    }
+}
